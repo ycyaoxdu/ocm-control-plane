4e7c6132617d3068358f048d4d3234e29ecab91d
ycyaoxdu <yaoyuchen0626@163.com>
2022-07-14 17:19:37 +0800

    remove resource: webhook service and apiservice
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- config/webhook/bootstrap.go
+++ config/webhook/bootstrap.go
@@ -15,6 +15,8 @@ import (
 )
 
 var WebhookSA = "managedcluster-admission-sa"
+var ServiceName = "managedcluster-admission"
+var HubNamespace = "open-cluster-management-hub"
 
 //go:embed *.yaml
 var fs embed.FS
@@ -31,6 +33,30 @@ func Bootstrap(ctx context.Context, discoveryClient discovery.DiscoveryInterface
 		// nolint:nilerr
 		return nil // don't klog.Fatal. This only happens when context is cancelled.
 	}
+
+	// selector := make(map[string]string)
+	// selector["app"] = ServiceName
+	// p := &corev1.ServicePort{
+	// 	Port:       443,
+	// 	TargetPort: intstr.FromInt(6443),
+	// }
+
+	// ser := &corev1.Service{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name:      ServiceName,
+	// 		Namespace: HubNamespace,
+	// 	},
+	// 	Spec: corev1.ServiceSpec{
+	// 		Selector: selector,
+	// 		Ports:    []corev1.ServicePort{*p},
+	// 	},
+	// }
+	// _, err = kubeClient.CoreV1().Services(HubNamespace).Create(ctx, ser, metav1.CreateOptions{})
+	// if err != nil {
+	// 	klog.Errorf("failed to bootstrap hub serviceaccount: %v", err)
+	// 	// nolint:nilerr
+	// 	return nil // don't klog.Fatal. This only happens when context is cancelled.
+	// }
 
 	return bootstrap(ctx, discoveryClient, dynamicClient)
 }
