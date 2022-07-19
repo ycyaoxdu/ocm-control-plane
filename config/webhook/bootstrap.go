a0778f507010e0a1391aafe39346231a03c40bbc
ycyaoxdu <yaoyuchen0626@163.com>
2022-07-19 13:27:56 +0800

    remove webhook resources
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- config/webhook/bootstrap.go
+++ config/webhook/bootstrap.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"embed"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/klog/v2"
 
 	confighelpers "open-cluster-management.io/ocm-controlplane/config/helpers"
 )
@@ -22,17 +19,17 @@ var HubNamespace = "open-cluster-management-hub"
 var fs embed.FS
 
 func Bootstrap(ctx context.Context, discoveryClient discovery.DiscoveryInterface, dynamicClient dynamic.Interface, kubeClient kubernetes.Interface) error {
-	var sa = &corev1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: WebhookSA,
-		},
-	}
-	_, err := kubeClient.CoreV1().ServiceAccounts("default").Create(ctx, sa, metav1.CreateOptions{})
-	if err != nil {
-		klog.Errorf("failed to bootstrap hub serviceaccount: %v", err)
-		// nolint:nilerr
-		return nil // don't klog.Fatal. This only happens when context is cancelled.
-	}
+	// var sa = &corev1.ServiceAccount{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name: WebhookSA,
+	// 	},
+	// }
+	// _, err := kubeClient.CoreV1().ServiceAccounts("default").Create(ctx, sa, metav1.CreateOptions{})
+	// if err != nil {
+	// 	klog.Errorf("failed to bootstrap hub serviceaccount: %v", err)
+	// 	// nolint:nilerr
+	// 	return nil // don't klog.Fatal. This only happens when context is cancelled.
+	// }
 
 	// selector := make(map[string]string)
 	// selector["app"] = ServiceName
